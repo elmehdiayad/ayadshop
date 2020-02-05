69c6d090b250e57b04ebc32adeb6e5ac0049ea5e
elmehdiayad <ay.elmehdi@gmail.com>
2020-02-05 11:59:26 +0100

    building the collections component

---
+++ src/pages/shop/shop-page.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import SHOP_DATA from './shop-page.data'
+import CollectionPreview from '../../component/collection-preview/collection-preview.component'
+
+
+class ShopPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      collections : SHOP_DATA
+    };
+  }
+
+
+  render(){
+    const collections = this.state.collections;
+    return (
+      <div className='shop-page'>
+        <h2>Collections</h2>
+        {collections.map(({id,...OtherProps}) => (<CollectionPreview key={id} {...OtherProps}/>))}
+      </div>
+      
+    );
+  }
+}
+
+
+export default ShopPage;
